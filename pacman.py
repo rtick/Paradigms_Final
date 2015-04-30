6e86433b21b700037b956147c5d11f16d4d0edc5
rtick <rtick@nd.edu>
2015-04-30 15:33:50 -0400

    Initialized some basic graphics. Loads background image and pacman starts in the center. He can move up, down, left, and right but collision detection is not yet implemented.

---
+++ pacman.py
@@ -0,0 +1,83 @@
+import sys, os, math, time, pygame
+from pygame.locals import *
+
+class Background(pygame.sprite.Sprite):
+	def __init__(self, gs=None):
+		self.gs = gs
+		FILE = "images/board.png"
+		self.image = pygame.image.load(FILE)
+		self.rect = self.image.get_rect()
+
+		
+class Player(pygame.sprite.Sprite):
+		
+	def __init__(self, gs=None):
+		self.gs = gs
+		FILE = "images/pacman.png"
+		self.image = pygame.image.load(FILE)
+		self.image = pygame.transform.scale(self.image, (int(25),int(25)))
+		
+		self.rect = self.image.get_rect()
+		self.rect.x = 396
+		self.rect.y = 398
+				
+	def move(self, keycode):
+		if (keycode == K_RIGHT):
+			self.rect = self.rect.move(2, 0)
+			
+		if (keycode == K_LEFT):
+			self.rect = self.rect.move(-2,0)
+			
+		if (keycode == K_DOWN):
+			self.rect = self.rect.move(0,2)
+			
+		if (keycode == K_UP):
+			self.rect = self.rect.move(0,-2)
+		return
+		
+	def tick(self):
+
+		#mx, my = pygame.mouse.get_pos()
+		#print mx, my
+
+		return
+		
+class GameSpace:
+	def main(self):
+		pygame.init()
+		
+		winTitle = pygame.display.set_caption("Paradigms Final Project: Multiplayer Pacman")
+		
+		self.size = self.width, self.height = 800, 800
+		
+		self.black = 0, 0, 0
+		
+		self.screen = pygame.display.set_mode(self.size)
+		
+		pygame.key.set_repeat(1, 50)
+		
+		self.clock = pygame.time.Clock()
+		
+		self.player = Player(self)
+		self.background = Background(self)
+		
+		while 1:
+			self.clock.tick(60)
+			
+			for event in pygame.event.get():
+				if (event.type == KEYDOWN):
+					self.player.move(event.key)
+			
+				if (event.type == pygame.QUIT):
+					sys.exit()
+				self.player.tick()
+				
+				self.screen.fill(self.black)	
+				self.screen.blit(self.background.image, self.background.rect)
+				self.screen.blit(self.player.image, self.player.rect)
+			
+				pygame.display.flip()
+			
+if __name__ == '__main__':
+	gs = GameSpace()
+	gs.main()
